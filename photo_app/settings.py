c2985e12e86d8160acd8d5140f07885e1cb3edbc
Edwin-metto <edwinmetto89@gmail.com>
2025-01-18 13:57:28 +0300

    database

--- photo_app/settings.py
+++ photo_app/settings.py
@@ -106,6 +106,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER':'gallery',
+        'PASSWORD':'123456',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
