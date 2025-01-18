46de77248b67d298f2101d0c58eab96f2a7c7c84
Edwin-metto <edwinmetto89@gmail.com>
2025-01-18 14:54:03 +0300

    styling

--- gallery/views.py
+++ gallery/views.py
@@ -1,46 +1,53 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 from .forms import ImageUploadForm
 from .models import Image
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, authenticate  
-from django.contrib.auth.decorators import login_required
-
 
 def home(request):
+    
     if request.user.is_authenticated:
-        return redirect('some_authenticated_page')
+        return redirect('upload_image')  
     else:
-        return redirect('register')
+        return redirect('login')  
+
 
 def register(request):
+    
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)  
-            return redirect('some_authenticated_page')  
+            return redirect('upload_image')  
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
 
-@login_required
-def profile(request):
-    return render(request, 'registration/profile.html')
 
+@login_required
 def upload_image(request):
-    if request.method == 'POST' and request.FILES['image']:
+
+    if request.method == 'POST' and request.FILES.get('image'):
         form = ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('image_list')  
     else:
         form = ImageUploadForm()
-
     return render(request, 'upload_image.html', {'form': form})
 
+
+@login_required
 def image_list(request):
+
     images = Image.objects.all()
-    return render(request, 'image_list.html', {'images': images})
+    return render(request, 'gallery/image_list.html', {'images': images})
+@login_required
+def profile(request):
+    return render(request, 'registration/profile.html')
+
 
 def like_image(request, image_id):
     image = Image.objects.get(id=image_id)
--- photo_app/urls.py
+++ photo_app/urls.py
@@ -18,14 +18,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from gallery.views import home
 
 urlpatterns = [
-    
-    path('admin/', admin.site.urls),  
-    path('images/', include('gallery.urls')), 
-    
+    path('', home, name='home'),  
+    path('admin/', admin.site.urls),
+    path('images/', include('gallery.urls')),  
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
